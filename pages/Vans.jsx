05b7758cdaf506ba9f240e57d99e8b22cd08190e
AdamKubalek <stsaffp@email.cz>
2023-04-26 16:11:29 +0200

    Van css

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -25,6 +25,7 @@ const Vans = () => {
 
   return (
     <div className="van-list-container">
+      <h1>Explore our van options</h1>
       <div className="van-list">{vanElements}</div>
     </div>
   );
